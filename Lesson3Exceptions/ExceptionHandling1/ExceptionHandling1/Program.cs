c030bb6074e87452f973b63cac8219a453927608
Aliaksandr Hryharuk <Aliaksandr_Hryharuk@epam.com>
2017-04-26 11:18:52 +0300

    Added ExceptionHandling console project

---
+++ Lesson3Exceptions/ExceptionHandling1/ExceptionHandling1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Напишите консольное приложение, которое выводит на экран первый из введенных символов каждой строки ввода.
+			//Опишите корректное поведение приложения, если пользователь ввел пустую строку.
+			Console.WriteLine("Enter your lines of the text. Type '-q' for the exit ");
+			StringBuilder sb = new StringBuilder();
+			try
+			{
+				while (true)
+				{
+
+					var str = Console.ReadLine();
+					if (string.IsNullOrWhiteSpace(str))
+					{
+						throw  new NullReferenceException();
+					}
+					if (str.IndexOf("-q", StringComparison.Ordinal) == -1)
+					{
+						sb.Append(str[0]);
+						sb.Append(" ");
+					}
+					else
+					{
+						break;
+					}
+				}
+
+			}
+			catch (NullReferenceException nullEx)
+			{
+				Console.WriteLine("We catched a null reference" + nullEx.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Result is: ");
+				Console.WriteLine(sb.ToString());
+				Console.ReadKey();
+			}
+		}
+	}
+}
