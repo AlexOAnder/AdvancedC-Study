7e96a5597e742b92e46b9c48b3b0fee72ee9868d
alexoander <thanksforsource@gmail.com>
2017-07-03 21:22:39 +0300

    Update cashing samples

--- Lesson10Caching/Samples/Application/CachingSolutionsSamples/CacheTests.cs
+++ Lesson10Caching/Samples/Application/CachingSolutionsSamples/CacheTests.cs
@@ -6,7 +6,10 @@ using System.Threading;
 
 namespace CachingSolutionsSamples
 {
-	[TestClass]
+    using CachingSolutionsSamples.Employees;
+    using CachingSolutionsSamples.Order;
+
+    [TestClass]
 	public class CacheTests
 	{
 		[TestMethod]
@@ -19,7 +22,23 @@ namespace CachingSolutionsSamples
 				Console.WriteLine(categoryManager.GetCategories().Count());
 				Thread.Sleep(100);
 			}
-		}
+
+            var employeeManager = new EmployeesManager(new EmployeesMemoryCache());
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(employeeManager.GetEmployee().Count());
+                Thread.Sleep(100);
+            }
+
+            var ordersManager = new OrderManager(new OrdersMemoryCache());
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(ordersManager.GetOrders().Count());
+                Thread.Sleep(100);
+            }
+        }
 
 		[TestMethod]
 		public void RedisCache()
@@ -31,6 +50,22 @@ namespace CachingSolutionsSamples
 				Console.WriteLine(categoryManager.GetCategories().Count());
 				Thread.Sleep(100);
 			}
-		}
+
+            var employeeManager = new EmployeesManager(new EmployeesRedisCache("localhost"));
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(employeeManager.GetEmployee().Count());
+                Thread.Sleep(100);
+            }
+
+            var ordersManager = new OrderManager(new OrdersRedisCache("localhost"));
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(ordersManager.GetOrders().Count());
+                Thread.Sleep(100);
+            }
+        }
 	}
 }
---
+++ Lesson10Caching/Samples/Application/CachingSolutionsSamples/Order/OrdersMemoryCache.cs
@@ -0,0 +1,46 @@
+﻿namespace CachingSolutionsSamples.Order
+{
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data.SqlClient;
+    using System.Runtime.Caching;
+
+    using NorthwindLibrary;
+
+    public class OrdersMemoryCache : IOrderCache
+    {
+        ObjectCache cache = MemoryCache.Default;
+        string prefix = "Cache_Orders";
+
+        public IEnumerable<Order> Get(string forUser)
+        {
+            return (IEnumerable<Order>)cache.Get(prefix + forUser);
+        }
+
+        public void Set(string forUser, IEnumerable<Order> orders)
+        {
+            var policy = new CacheItemPolicy();
+            string connStr = ConfigurationManager.ConnectionStrings["Northwind"].ConnectionString;
+            SqlDependency.Start(connStr);
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand command = new SqlCommand("Select * From Orders", conn))
+                {
+                    command.Notification = null;
+
+                    SqlDependency dep = new SqlDependency();
+
+                    dep.AddCommandDependency(command);
+
+                    conn.Open();
+
+                    SqlChangeMonitor monitor = new SqlChangeMonitor(dep);
+
+                    policy.ChangeMonitors.Add(monitor);
+                }
+
+                cache.Set(prefix + forUser, orders, policy);
+            }
+        }
+    }
+}
