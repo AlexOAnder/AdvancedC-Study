6e6927f2bf8099799281efc754c00bdb5c23f57a
alexoander <thanksforsource@gmail.com>
2017-07-11 11:17:55 +0300

    update

---
+++ Lesson10Caching/Samples/Application/CachingSolutionsSamples/XmlDataContractSerializerTester.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+
+namespace Task.TestHelpers
+{
+	public class XmlDataContractSerializerTester<T> : SerializationTester<T, XmlObjectSerializer>
+	{
+		public XmlDataContractSerializerTester(
+			XmlObjectSerializer serializer, bool showResult = false) : base(serializer, showResult)
+		{ }
+
+		internal override T Deserialization(MemoryStream stream)
+		{
+			return (T)serializer.ReadObject(stream);
+		}
+
+		internal override void Serialization(T data, MemoryStream stream)
+		{
+			serializer.WriteObject(stream, data);
+		}
+	}
+
+	public class SerializationTester<T, T1>
+	{
+		void Serialization(T data, MemoryStream stream)
+		{
+		}
+
+		void Deserialization(MemoryStream stream)
+		{
+		}
+
+		public void SerializeAndDeserialize(T model)
+		{
+		}
+	}
+}
--- Lesson10Caching/Samples/Application/NorthwindLibrary/Category.cs
+++ Lesson10Caching/Samples/Application/NorthwindLibrary/Category.cs
@@ -1,3 +1,5 @@
+using System.Runtime.Serialization;
+
 namespace NorthwindLibrary
 {
     using System;
@@ -6,6 +8,7 @@ namespace NorthwindLibrary
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
 
+	[DataContract(Name = "CATEGORY", Namespace = "")]
     public partial class Category
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -13,17 +16,20 @@ namespace NorthwindLibrary
         {
             Products = new HashSet<Product>();
         }
-
+		[DataMember(Name = "ID", Order = 0)]
         public int CategoryID { get; set; }
 
         [Required]
         [StringLength(15)]
+		[DataMember(Name = "NAME", Order = 1)]
         public string CategoryName { get; set; }
 
         [Column(TypeName = "ntext")]
+		[DataMember(Name = "DESCRIPTION", Order = 2)]
         public string Description { get; set; }
 
-        [Column(TypeName = "image")]
+        [Column(TypeName = "PICTURE")]
+		[DataMember(Name = "STREET_NUM", Order = 3)]
         public byte[] Picture { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
